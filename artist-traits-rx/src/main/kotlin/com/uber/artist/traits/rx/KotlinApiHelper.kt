11e998121e24ddd87d54e07644df51af46faebdf
Zac Sweers <pandanomic@gmail.com>
2019-01-09 03:13:33 -0800

    Fix accidental brackets

--- artist-traits-rx/src/main/kotlin/com/uber/artist/traits/rx/KotlinApiHelper.kt
+++ artist-traits-rx/src/main/kotlin/com/uber/artist/traits/rx/KotlinApiHelper.kt
@@ -133,7 +133,7 @@ fun addRxBindingApiForSettable(type: TypeSpec.Builder, api: KotlinSettableApi, i
             }.append("@deprecated Use [$rxBindingMethod]\n").toString())
             .addModifiers(KModifier.FINAL, KModifier.OVERRIDE)
             .addAnnotation(AnnotationSpec.builder(Deprecated::class.java)
-                .addMember("message = %S", "Use [$rxBindingMethod()]")
+                .addMember("message = %S", "Use $rxBindingMethod()")
                 .addMember("replaceWith = %T(%S)", ReplaceWith::class.asClassName(), "$rxBindingMethod()")
                 .addMember("level = %T.ERROR", DeprecationLevel::class.asClassName())
                 .build())
